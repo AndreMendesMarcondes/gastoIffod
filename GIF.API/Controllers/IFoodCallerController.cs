4bb2a44f741767cef8e4d27aa987ce00feab2d7f
André Mendes <andre.marcondes@betalearning.com.br>
2021-09-20 10:01:41 -0300

    criando aplicacao de api

---
+++ GIF.API/Controllers/IFoodCallerController.cs
@@ -0,0 +1,34 @@
+﻿using GIF.Domain;
+using GIF.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GIF.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IFoodCallerController : ControllerBase
+    {
+        private readonly IIFoodAPI _ifoodAPI;
+
+        public IFoodCallerController(IIFoodAPI ifoodAPI)
+        {
+            _ifoodAPI = ifoodAPI;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string bearerToken)
+        {
+            IFoodTotalOrderDTO totalOrders = new();
+
+            if (!String.IsNullOrEmpty(bearerToken))
+            {
+                bearerToken = bearerToken.Replace("Bearer ", "");
+                totalOrders = _ifoodAPI.GetOrders(bearerToken);
+            }
+
+            totalOrders.Bearer = bearerToken;
+            return Ok(totalOrders);
+        }
+    }
+}
---
+++ GIF.API/Startup.cs
@@ -0,0 +1,56 @@
+using GIF.Service.Imp;
+using GIF.Service.Interface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace GIF.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GIF.API", Version = "v1" });
+            });
+
+            services.AddSingleton<IIFoodAPI, IFoodAPI>();
+            services.AddSingleton<IIFoodBusiness, IFoodBusiness>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GIF.API v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
